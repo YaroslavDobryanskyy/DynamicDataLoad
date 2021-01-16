7acdebe5e30877798609f2ee52bf39045818aa9b
Yaroslav Dobryanskyy <ydobryan@softserveinc.com>
2021-01-16 23:30:17 +0200

    Added Demo sp for BULK INSERT

---
+++ DynamicDataLoadSSIS/DynamicDataLoadDB/Procedures/BulkInsertTextFile.sql
@@ -0,0 +1,17 @@
+﻿-- EXEC [dbo].[BulkInsertTextFile] @TargetTable ='Stage.VEmployee', @SourceFile = 'C:\Temp\Intermarket\DataSeed\Employee.txt'
+
+CREATE PROCEDURE [dbo].[BulkInsertTextFile]
+	@TargetTable nvarchar(100),
+	@SourceFile nvarchar(250),
+	@FieldTerminator nchar(1) = ','
+AS
+BEGIN
+
+DECLARE @SQLQuery nvarchar(max)
+SET @SQLQuery = 'BULK INSERT '  + @TargetTable + ' FROM ''' + @SourceFile +
+''' WITH ( FIELDTERMINATOR = ''' + @FieldTerminator+ ''', ROWTERMINATOR = ''\n'')'
+EXEC(@SQLQuery)
+
+
+RETURN 0
+END
